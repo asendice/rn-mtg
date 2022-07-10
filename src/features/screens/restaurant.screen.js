47a8e52e8161fcbe12afc7f4dac1203424c83235
Dylan Travis <dylan.travis23@outlook.com>
2022-07-09 21:33:23 -0700

    app take over from failed proj

---
+++ src/features/screens/restaurant.screen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+import styled from "styled-components/native";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `marginTop: ${StatusBar.currentHeight}px`}
+`;
+const SearchContainer = styled(View)`
+  padding: 16px;
+`;
+const RestaurantListContainer = styled(View)`
+  flex: 1;
+  padding: 16px;
+`;
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar placeholder="Search" />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
+};
